703d9e58758118ae3f1656b1eefb63c8390b9922
TomAndy <andrufly@ukr.net>
2016-04-27 13:49:15 +0300

    Update

--- lesson2-1/task4.py
+++ lesson2-1/task4.py
@@ -1,9 +1,14 @@
 # print 'Write a program to calculate the number of times that the string \'hi\' appears anywhere in the given string ' \
 #       'and change \'hi\' to \'bye\'. Case should be ignored'
 
-str1='hi. Today is hi again. I can\'t stand using hi in any string'
+import re
 
-x=str1.count('hi')
+str1='hI. Today is Hi again. I can\'t stand using hi in any string'
+
+x=str1.upper().count("hi".upper())
 print x
-str2=str1.replace('hi','bye')
+str2=str1.replace('hi','bye').replace('Hi','bye').replace('HI','bye').replace('hI','bye')
 print str2
+
+insens_str=re.compile(re.escape('hi'),re.IGNORECASE)
+print insens_str.sub('bye',str1)
