a581a94418a3a13a717904c873b5368e80ba6081
TomAndy <andrufly@ukr.net>
2017-04-10 23:01:20 +0300

    modified movie_score file

--- hometask2/movie_score.py
+++ hometask2/movie_score.py
@@ -1,41 +1,53 @@
 
 import omdb
+import csv
 
 
-path_to_input_file = 'd:\My\Python\homework_from_Orest_Voloshchuk\hw02\movie_names2'
-path_to_good_movies_file = ''
-path_to_invalid_titles_file = ''
-path_to_invalid_metascore_file = ''
+path_to_input_file = 'movie_names2'
+path_to_good_movies_file = 'rating.csv'
+path_to_invalid_titles_file = 'invalid_titles.txt'
+path_to_invalid_metascore_file = 'invalid_metascore.txt'
 
 
-def getMovieData(pathToFile):
-    movieData = {}
-    i=0
+def readDataFromFileIntoList(pathToFile):
+    movieList = []
     with open(pathToFile) as inputFile:
         for line in inputFile:
-            i=i+1
-            print i,":  "+line
-            dataItem = omdb.search(line.rstrip())
-            movieData.update({line.rstrip():dataItem or ''})
+            movieList.append(line)
     inputFile.close()
+    return movieList
+
+
+def getMovieData(movieList):
+    movieData = {}
+    i=0
+    for item in movieList:
+        i=i+1
+        print i,":  "+item
+        dataItem = omdb.search(str(item).rstrip())
+        movieData.update({str(item).rstrip(): dataItem or ''})
     return movieData
 
 
 def getMovieScore(movieData):
+    print '###################'
+    print 'Now we are calculating the score'
     invalid_metascore = []
     good_movies = []
+    i=0
     for key, value in movieData.iteritems():
+        i = i + 1
+        print i
         if value == '':
             continue
         else:
             for movieMetaData in value:
                 if movieMetaData['title'].encode('utf-8') == key and (movieMetaData['type'] == 'movie' or movieMetaData['type'] == 'series'):
                     imdb_metascore = omdb.imdbid(movieMetaData['imdb_id'])['metascore'] or ''
-                    print "score: ", imdb_metascore
                     if imdb_metascore == 'N/A' or not imdb_metascore.isdigit():
                         invalid_metascore.append(key)
                     else:
-                        good_movies.append({key, imdb_metascore})
+                        good_movies.append({key : imdb_metascore})
                     break
     # print len(invalid_metascore)
     # print len(good_movies)
@@ -52,25 +64,31 @@ def getInvalidTitles(movieData):
 
 if __name__ == '__main__':
 
-    movieData = getMovieData(path_to_input_file)  # get dict of movie_title with metadata from input file
+    movieList = readDataFromFileIntoList(path_to_input_file)
+    movieData = getMovieData(movieList)  # get dict of movie_title with metadata from input file
     invalid_titles = getInvalidTitles(movieData)
     invalid_metascore = getMovieScore(movieData)[0]
     good_movies = getMovieScore(movieData)[1]
 
-    with open('invalid_metascore','w') as invalid_m:
+    with open(path_to_invalid_metascore_file,'w') as invalid_m:
         for item in invalid_metascore:
             invalid_m.write(item+'\n')
     invalid_m.close()
 
-    with open('invalid_title','w') as invalid_t:
+    with open(path_to_invalid_titles_file,'w') as invalid_t:
         for item in invalid_titles:
             invalid_t.write(item+'\n')
     invalid_t.close()
 
-    with open('good_movies','w') as good_m:
-        for k,v in good_movies:
-            good_m.write("\""+k+",\" "+v +'\n')
+    with open(path_to_good_movies_file,'wb') as good_m:
+        writer = csv.writer(good_m, delimiter=',')
+        writer.writerow(('Title', 'Metascore'))
+        for item in good_movies:
+            for key in item:
+                writer.writerow((key, ' ' + item[key]))
     good_m.close()
+
+
 
 
 
